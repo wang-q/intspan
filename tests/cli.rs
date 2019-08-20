1e309b5809cd5d562f1328fc4494f9c91475159c
wang-q <wang-q@outlook.com>
2019-08-20 08:11:30 +0800

    Misc changes

--- tests/cli.rs
+++ tests/cli.rs
@@ -14,6 +14,29 @@ fn command_invalid() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+fn file_doesnt_be_needed() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+    cmd.arg("test")
+        .arg("tests/resources/S288c.chr.sizes");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("which wasn't expected, or isn't valid in this context"));
+
+    Ok(())
+}
+
+#[test]
+fn command_test() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+    cmd.arg("test");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("-30--21,-4-9,20-39,60-61,79-84,86,89-90,99"));
+
+    Ok(())
+}
+
+#[test]
 fn file_doesnt_provided() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     cmd.arg("genome");
