bd7eeec81d318c38d9a4cd4f113c19a7d48f1b8e
wang-q <wang-q@outlook.com>
2019-09-02 16:30:58 +0800

    Rename binary `intspan` to `spanr`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate clap;
 use clap::*;
 
-mod cmd;
+mod cmd_spanr;
 
 fn main() {
     let app = App::new("intspan")
@@ -9,35 +9,35 @@ fn main() {
         .author(crate_authors!())
         .about("`intspan` operates chromosome IntSpan files")
         .setting(AppSettings::ArgRequiredElseHelp)
-        .subcommand(cmd::genome::make_subcommand())
-        .subcommand(cmd::some::make_subcommand())
-        .subcommand(cmd::merge::make_subcommand())
-        .subcommand(cmd::split::make_subcommand())
-        .subcommand(cmd::stat::make_subcommand())
-        .subcommand(cmd::statop::make_subcommand())
-        .subcommand(cmd::combine::make_subcommand())
-        .subcommand(cmd::compare::make_subcommand())
-        .subcommand(cmd::span::make_subcommand())
-        .subcommand(cmd::cover::make_subcommand())
-        .subcommand(cmd::gff::make_subcommand())
-        .subcommand(cmd::convert::make_subcommand())
-        .subcommand(cmd::range::make_subcommand());
+        .subcommand(cmd_spanr::genome::make_subcommand())
+        .subcommand(cmd_spanr::some::make_subcommand())
+        .subcommand(cmd_spanr::merge::make_subcommand())
+        .subcommand(cmd_spanr::split::make_subcommand())
+        .subcommand(cmd_spanr::stat::make_subcommand())
+        .subcommand(cmd_spanr::statop::make_subcommand())
+        .subcommand(cmd_spanr::combine::make_subcommand())
+        .subcommand(cmd_spanr::compare::make_subcommand())
+        .subcommand(cmd_spanr::span::make_subcommand())
+        .subcommand(cmd_spanr::cover::make_subcommand())
+        .subcommand(cmd_spanr::gff::make_subcommand())
+        .subcommand(cmd_spanr::convert::make_subcommand())
+        .subcommand(cmd_spanr::range::make_subcommand());
 
     // Check which subcomamnd the user ran...
     let _res = match app.get_matches().subcommand() {
-        ("genome", Some(sub_matches)) => cmd::genome::execute(sub_matches),
-        ("some", Some(sub_matches)) => cmd::some::execute(sub_matches),
-        ("merge", Some(sub_matches)) => cmd::merge::execute(sub_matches),
-        ("split", Some(sub_matches)) => cmd::split::execute(sub_matches),
-        ("stat", Some(sub_matches)) => cmd::stat::execute(sub_matches),
-        ("statop", Some(sub_matches)) => cmd::statop::execute(sub_matches),
-        ("combine", Some(sub_matches)) => cmd::combine::execute(sub_matches),
-        ("compare", Some(sub_matches)) => cmd::compare::execute(sub_matches),
-        ("span", Some(sub_matches)) => cmd::span::execute(sub_matches),
-        ("cover", Some(sub_matches)) => cmd::cover::execute(sub_matches),
-        ("gff", Some(sub_matches)) => cmd::gff::execute(sub_matches),
-        ("convert", Some(sub_matches)) => cmd::convert::execute(sub_matches),
-        ("range", Some(sub_matches)) => cmd::range::execute(sub_matches),
+        ("genome", Some(sub_matches)) => cmd_spanr::genome::execute(sub_matches),
+        ("some", Some(sub_matches)) => cmd_spanr::some::execute(sub_matches),
+        ("merge", Some(sub_matches)) => cmd_spanr::merge::execute(sub_matches),
+        ("split", Some(sub_matches)) => cmd_spanr::split::execute(sub_matches),
+        ("stat", Some(sub_matches)) => cmd_spanr::stat::execute(sub_matches),
+        ("statop", Some(sub_matches)) => cmd_spanr::statop::execute(sub_matches),
+        ("combine", Some(sub_matches)) => cmd_spanr::combine::execute(sub_matches),
+        ("compare", Some(sub_matches)) => cmd_spanr::compare::execute(sub_matches),
+        ("span", Some(sub_matches)) => cmd_spanr::span::execute(sub_matches),
+        ("cover", Some(sub_matches)) => cmd_spanr::cover::execute(sub_matches),
+        ("gff", Some(sub_matches)) => cmd_spanr::gff::execute(sub_matches),
+        ("convert", Some(sub_matches)) => cmd_spanr::convert::execute(sub_matches),
+        ("range", Some(sub_matches)) => cmd_spanr::range::execute(sub_matches),
         (_, _) => unreachable!(),
     };
 }
