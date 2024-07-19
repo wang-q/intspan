053d950ccb469e91c43b7e3b1b47fde02a4b5c93
Qiang Wang <wang-q@outlook.com>
2024-07-20 06:44:35 +0800

    Update cli_rgr.rs

--- tests/cli_rgr.rs
+++ tests/cli_rgr.rs
@@ -436,9 +436,9 @@ fn command_pl_2rmp() -> anyhow::Result<()> {
         .arg(tempdir_str.to_owned() + "/replaced.tsv")
         .output()
         .unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
+    let stderr = String::from_utf8(output.stderr).unwrap();
 
-    assert!(stdout.lines().count() > 1);
+    assert!(stderr.lines().count() > 1);
     // assert!(&tempdir.path().join("replaced.tsv").is_file());
 
     tempdir.close()?;
